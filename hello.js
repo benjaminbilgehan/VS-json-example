b076f742ec3184231bcfb22c2f0b7efe6d29a615
bilgehanevren <bilgehanevren@gmail.com>
2022-01-16 17:06:34 +0200

    first commit

---
+++ hello.js
@@ -0,0 +1,14 @@
+
+function helloWorld() {
+  process.stdout.write("Hello World\n");
+}
+
+function main(outputNum) {
+ for (let i=0; i< outputNum; i++) {
+   helloWorld();
+ }
+}
+
+const args = process.argv.slice(2);
+
+main(args[0]);
