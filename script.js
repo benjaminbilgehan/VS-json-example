147b2f91615e2e6eb5216e2336517318e07d0bb9
bilgehanevren <bilgehanevren@gmail.com>
2022-01-24 06:25:35 +0200

    init

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-let companies = 
-[
-    {
-        "name": "Big Corporation",
-        "numberOfEmployees": 10000,
-        "ceo": "Mary",
-        "rating":3.6
-    },
-    {
-        "name": "Small Startup",
-        "numberOfEmployees":3,
-        "ceo":null,
-        "rating":4.3
-    }
-    
-    ]
-console.log(companies)
-    
+import 'boxicons'
